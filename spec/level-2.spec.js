002fad6d7e23e496648d911c3c8ac4014b675242
davified <davified@gmail.com>
2017-03-09 10:00:29 +0800

    Translate category-1 double-digit integers to English equivalent

--- spec/level-2.spec.js
+++ spec/level-2.spec.js
@@ -7,7 +7,7 @@ var numberTranslator = new NumberTranslator()
 // Write a JavaScript program to calculate the factorial of a number.
 // In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
 
-describe('Translating single integers to English equivalent', function () {
+describe('Translating single-digit integers to English equivalent', function () {
   it('should return One', function () {
     var expected = 'One'
     var actual = numberTranslator.translateToEnglish(1)
@@ -29,6 +29,20 @@ describe('Translating single integers to English equivalent', function () {
   it('should return Nine', function () {
     var expected = 'Nine'
     var actual = numberTranslator.translateToEnglish(9)
+    expect(expected).to.be.eql(actual)
+  })
+})
+
+describe('Translating "category-1" double-digit integers to English equivalent', function () {
+  it('should return Ten', function () {
+    var expected = 'Ten'
+    var actual = numberTranslator.translateToEnglish(10)
+    expect(expected).to.be.eql(actual)
+  })
+
+	it('should return Twelve', function () {
+    var expected = 'Twelve'
+    var actual = numberTranslator.translateToEnglish(12)
     expect(expected).to.be.eql(actual)
   })
 })
