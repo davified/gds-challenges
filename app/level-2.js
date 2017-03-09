f9cd14925b778b5616810983a1e72946f1454da8
davified <davified@gmail.com>
2017-03-09 09:55:48 +0800

    Create map for single-digit integers

--- app/level-2.js
+++ app/level-2.js
@@ -14,7 +14,7 @@
 
 class NumberTranslator {
   translateToEnglish (input) {
-    var map = {1: 'one', 2: 'two'}
+    var map = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
 
     var output = map[input]
     var capitalizedOutput = output.charAt(0).toUpperCase() + output.slice(1)
--- spec/level-2.spec.js
+++ spec/level-2.spec.js
@@ -7,7 +7,7 @@ var numberTranslator = new NumberTranslator()
 // Write a JavaScript program to calculate the factorial of a number.
 // In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
 
-describe('Collpasing single strings', function () {
+describe('Translating single integers to English equivalent', function () {
   it('should return One', function () {
     var expected = 'One'
     var actual = numberTranslator.translateToEnglish(1)
@@ -17,6 +17,18 @@ describe('Collpasing single strings', function () {
 	it('should return Two', function () {
     var expected = 'Two'
     var actual = numberTranslator.translateToEnglish(2)
+    expect(expected).to.be.eql(actual)
+  })
+
+	it('should return Eight', function () {
+    var expected = 'Eight'
+    var actual = numberTranslator.translateToEnglish(8)
+    expect(expected).to.be.eql(actual)
+  })
+
+	it('should return Nine', function () {
+    var expected = 'Nine'
+    var actual = numberTranslator.translateToEnglish(9)
     expect(expected).to.be.eql(actual)
   })
 })
