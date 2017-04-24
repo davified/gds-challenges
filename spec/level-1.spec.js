f09961c5ba5812d94dca8594c9c3c052c7494a1e
David Tan <davified@GMAIL.COM>
2017-04-24 13:42:28 +0800

    Update level-1.spec.js

--- spec/level-1.spec.js
+++ spec/level-1.spec.js
@@ -4,9 +4,6 @@ var assert = require('assert')
 var StringCollapser = require('../app/level-1')
 var stringCollapser = new StringCollapser()
 
-// Write a JavaScript program to calculate the factorial of a number.
-// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example, 5! = 5 x 4 x 3 x 2 x 1 = 120
-
 describe('Collpasing single strings', function () {
   it('should return Oo', function () {
     var expected = 'Oo'
